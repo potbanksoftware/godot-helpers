c7c44facc05c5e409135bff98872323d80edd505
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2024-08-20 20:11:39 +0100

    Add pre-commit and gdlint config

--- controls/volume_slider/volume_slider.gd
+++ controls/volume_slider/volume_slider.gd
@@ -9,13 +9,11 @@ signal value_changed
 		bus_name = value
 		_update_label_and_slider()
 
-
 @export var label_theme: Theme:
 	set(value):
 		%Label.theme = value
 	get:
 		return %Label.theme
-
 
 @export var slider_theme: Theme:
 	set(value):
@@ -32,17 +30,16 @@ var value: float:
 		%HSlider.value = volume
 
 
-
-
-
 func _ready() -> void:
 	_update_label_and_slider()
+
 
 func _update_label_and_slider():
 	$MarginContainer/VBoxContainer/Label.text = "%s Volume" % [bus_name]
 	if not Engine.is_editor_hint():
 		bus_index = AudioServer.get_bus_index(bus_name)
 		%HSlider.value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+
 
 func _on_h_slider_value_changed(_value: float) -> void:
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(%HSlider.value))

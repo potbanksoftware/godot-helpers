8201bb5d7b935e87854088741c792bc693ae713f
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2024-08-20 19:29:13 +0100

    Volume slider: update label and slider position when bus name changed.

--- controls/volume_slider/volume_slider.gd
+++ controls/volume_slider/volume_slider.gd
@@ -4,7 +4,10 @@ extends MenuControl
 
 signal value_changed
 
-@export var bus_name: String
+@export var bus_name: String:
+	set(value):
+		bus_name = value
+		_update_label_and_slider()
 
 
 @export var label_theme: Theme:
@@ -33,11 +36,13 @@ var value: float:
 
 
 func _ready() -> void:
+	_update_label_and_slider()
+
+func _update_label_and_slider():
 	$MarginContainer/VBoxContainer/Label.text = "%s Volume" % [bus_name]
 	if not Engine.is_editor_hint():
 		bus_index = AudioServer.get_bus_index(bus_name)
 		slider.value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
-
 
 func _on_h_slider_value_changed(_value: float) -> void:
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(slider.value))

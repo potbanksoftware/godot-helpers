169eb75070a3ec759df214e81048b41df7ee9167
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2024-09-05 09:36:18 +0100

    Remove debug print

--- blur/blur_colour_rect.gd
+++ blur/blur_colour_rect.gd
@@ -2,7 +2,6 @@ extends ColorRect
 
 @export var lod: float= 0.75:
 	set(value):
-		print_debug(material.get_shader_parameter("lod"))
 		material.set_shader_parameter("lod", value)
 	get:
 		return material.get_shader_parameter("lod")

7a64619cf3133ccf98446a8a161f03bb9c0e4085
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-07-24 21:01:21 +0100

    Lint

--- controls/dropdown_button/dropdown_button.gd
+++ controls/dropdown_button/dropdown_button.gd
@@ -9,13 +9,6 @@ signal item_selected(index: int)
 		%Label.text = value
 	get:
 		return %Label.text
-
-var selected: int:
-	set(value):
-		%OptionButton.selected = value
-	get:
-		return %OptionButton.selected
-
 @export var label_theme: Theme:
 	set(value):
 		%Label.theme = value
@@ -28,19 +21,30 @@ var selected: int:
 	get:
 		return %OptionButton.theme
 
+var selected: int:
+	set(value):
+		%OptionButton.selected = value
+	get:
+		return %OptionButton.selected
+
+
 
 func _on_option_button_item_selected(index: int) -> void:
 	item_selected.emit(index)
 
 
-## Adds an item, with text label and (optionally) id. If no id is passed, the item index will be used as the item's ID. New items are appended at the end.
+## Adds an item, with text label and (optionally) id.
+## If no id is passed, the item index will be used as the item's ID. New items are appended at the end.
 ##
 ## Note: The item will be selected if there are no other items.
 func add_item(label: String, id: int = -1) -> void:
 	%OptionButton.add_item(label, id)
 
 
-## Adds a separator to the list of items. Separators help to group items, and can optionally be given a text header. A separator also gets an index assigned, and is appended at the end of the item list.
+## Adds a separator to the list of items.
+## Separators help to group items, and can optionally be given a text header.
+##
+## A separator also gets an index assigned, and is appended at the end of the item list.
 func add_separator(separator_text: String = "") -> void:
 	%OptionButton.add_separator(separator_text)
 
